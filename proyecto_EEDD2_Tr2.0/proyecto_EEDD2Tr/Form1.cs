8020f6b55bf1a913cff0e3b16ea9d3c6aaa50b31
MarcCP-XK <101739830+MarcCP-XK@users.noreply.github.com>
2023-02-28 20:38:38 +0100

    Add archivo que no dejaba a Luismi

---
+++ proyecto_EEDD2_Tr2.0/proyecto_EEDD2Tr/Form1.cs
@@ -0,0 +1,30 @@
+namespace proyecto_EEDD2Tr
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void tablaToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void reaccionesToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void compuestosToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
